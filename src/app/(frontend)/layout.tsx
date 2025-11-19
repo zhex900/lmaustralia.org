44843763786f3022ac92bea9356e17e5e7e4baff
Jake He <jake.he@nextgen.group>
2025-11-19 21:55:15 +1100

    Analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -12,6 +12,7 @@ import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
+import { Analytics } from '@vercel/analytics/next'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -49,6 +50,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           )}
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
