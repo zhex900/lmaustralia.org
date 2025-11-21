1ee09a75e62e9ab0e52bd1c10ce9d84f75bb104e
Jake He <jake.he@nextgen.group>
2025-11-21 21:32:38 +1100

    first migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251121_102631 from './20251121_102631';
+
+export const migrations = [
+  {
+    up: migration_20251121_102631.up,
+    down: migration_20251121_102631.down,
+    name: '20251121_102631'
+  },
+];
