46f66c01eada4f0ab57811b750fed491dd483fb7
Jake He <jake.he@nextgen.group>
2025-11-30 18:15:45 +1100

    css tweak

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -9,30 +9,29 @@ import { AustraliaMap } from '@/components/Maps'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
-    <div className="min-h-screen relative flex items-end">
-      <div className="absolute w-[95%] sm:w-[90%] md:w-[85%] lg:w-[70%] xl:w-[50%] 2xl:w-[45%] z-0 left-1/2 top-[30%] -translate-x-1/2 -translate-y-1/2  pointer-events-none">
+    <div className="h-dvh relative flex items-end">
+      <div className="absolute w-[95%] sm:w-[90%] md:w-[85%] lg:w-[70%] xl:w-[50%] 2xl:w-[45%] z-0 left-1/2 top-[30%] md:top-[35%] -translate-x-1/2 -translate-y-1/2  pointer-events-none">
         <AustraliaMap>
           {richText && (
             <div className=" max-w-3xl">
               <RichText className="mt-30 mx-10" data={richText} enableGutter={false} />
-              <div className="z-10 absolute -bottom-8 left-0 w-full flex items-center justify-center">
-                {Array.isArray(links) && links.length > 0 && (
-                  <ul className="flex justify-between w-[70%] md:w-[60%] lg:w-[50%]">
-                    {links.map(({ link }, i) => {
-                      return (
-                        <li key={i}>
-                          <CMSLink {...link} />
-                        </li>
-                      )
-                    })}
-                  </ul>
-                )}
-              </div>
             </div>
           )}
         </AustraliaMap>
       </div>
-
+      <div className="z-10 absolute bottom-58 left-0 w-full flex items-center justify-center">
+        {Array.isArray(links) && links.length > 0 && (
+          <ul className="flex justify-center gap-40">
+            {links.map(({ link }, i) => {
+              return (
+                <li key={i}>
+                  <CMSLink {...link} />
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </div>
       {media && typeof media === 'object' && (
         <Media fill imgClassName="-z-10 object-cover" resource={media} />
       )}
