081d5b468070fbae9e0b3b48b3692525fc9fe660
Jake He <jake.he@nextgen.group>
2025-12-10 18:43:20 +1100

    timeline adjust

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -68,7 +68,7 @@ export const Timeline = ({
           style={{
             height: height + 'px',
           }}
-          className="absolute md:left-6.5 left-6.5 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute left-6 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
