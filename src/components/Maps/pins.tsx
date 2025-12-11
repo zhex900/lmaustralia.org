19522da4ce819c77c63d5399173b2d0fa748ae3d
Jake He <jake.he@nextgen.group>
2025-12-12 10:07:12 +1100

    underline map city label

--- src/components/Maps/pins.tsx
+++ src/components/Maps/pins.tsx
@@ -12,7 +12,7 @@ export const pinPositions: {
     href: '/posts/newcastle',
     lat: -32.9283, // Newcastle, NSW latitude
     lng: 151.7817, // Newcastle, NSW longitude
-    labelOffsetX: -55,
+    labelOffsetX: -2,
   },
   {
     name: 'Cairns',
