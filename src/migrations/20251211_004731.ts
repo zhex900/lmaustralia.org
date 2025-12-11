5ba6bfca3281e209c03c5d5c3db73bf39c440258
Jake He <jake.he@nextgen.group>
2025-12-11 13:22:02 +1100

    update migration

--- src/migrations/20251211_004731.ts
+++ src/migrations/20251211_004731.ts
@@ -2,6 +2,10 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postg
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
+    ALTER TABLE "pages_blocks_timeline_timeline" DROP COLUMN IF EXISTS "content";
+    ALTER TABLE "_pages_v_blocks_timeline_timeline" DROP COLUMN IF EXISTS "content";
+  `)
+  await db.execute(sql`
    ALTER TABLE "pages_blocks_timeline_timeline" ADD COLUMN "content" jsonb;
   ALTER TABLE "_pages_v_blocks_timeline_timeline" ADD COLUMN "content" jsonb;`)
 }
@@ -10,4 +14,8 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   await db.execute(sql`
    ALTER TABLE "pages_blocks_timeline_timeline" DROP COLUMN "content";
   ALTER TABLE "_pages_v_blocks_timeline_timeline" DROP COLUMN "content";`)
+  await db.execute(sql`
+      ALTER TABLE "pages_blocks_timeline_timeline" ADD COLUMN "content" varchar;
+      ALTER TABLE "_pages_v_blocks_timeline_timeline" ADD COLUMN "content" varchar;
+    `)
 }
