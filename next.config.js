f5d0dd03808f475d3e303c6acfe80e64590b154e
Jake He <jake.he@nextgen.group>
2025-12-19 07:22:09 +1100

    disable HSTS temporary

--- next.config.js
+++ next.config.js
@@ -44,6 +44,19 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=0',
+          },
+        ],
+      },
+    ]
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '1gb',
