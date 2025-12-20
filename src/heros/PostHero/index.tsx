f21b62c5847edfb8ac38474f6546382e6fe2ce83
Jake He <jake.he@nextgen.group>
2025-12-21 08:01:01 +1100

    hero post text white

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -19,7 +19,9 @@ export const PostHero: React.FC<{
       <div className="container z-10 relative lg:grid lg:grid-cols-[1fr_48rem_1fr] text-white pb-8">
         <div className="col-start-1 col-span-1 md:col-start-2 md:col-span-2">
           <div className="">
-            <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl">{title}</h1>
+            <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl" style={{ color: '#ffffff' }}>
+              {title}
+            </h1>
           </div>
         </div>
       </div>
